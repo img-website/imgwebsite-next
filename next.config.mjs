230df0ba85f067479fc51bcb034b948cd5eb11f9
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2025-06-03 16:00:38 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,12 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'd1y41eupgbwbb2.cloudfront.net',
-        pathname: '/images/**',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'https://imgglobalinfotech-website.s3.ap-south-1.amazonaws.com',
+        pathname: '/**',
       },
     ],
   },
