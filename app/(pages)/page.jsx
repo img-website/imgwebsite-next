32b51e2a6c6f70033caf5d846b05fb1aabaf79f6
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2025-06-03 20:56:28 +0530

    Update page.jsx

--- app/(pages)/page.jsx
+++ app/(pages)/page.jsx
@@ -1,37 +1,37 @@
 export const dynamic = 'force-static';
 
 
-import HomeHero from '@/components/HomeHero';
+// import HomeHero from '@/components/HomeHero';
 import CoreServices from '@/components/HomePage/CoreServices';
-import CaseStudy from '@/components/HomePage/CaseStudy';
+// import CaseStudy from '@/components/HomePage/CaseStudy';
 import Brands from '@/components/HomePage/Brands';
 import Cta1 from '@/components/cta/Cta1';
-import Portfolio1 from '@/components/portfolios/Portfolio1';
+// import Portfolio1 from '@/components/portfolios/Portfolio1';
 import AdvanceTech from '@/components/HomePage/AdvanceTech';
 import Industries from '@/components/HomePage/Industries';
 import QuickInsight from '@/components/HomePage/QuickInsight';
 import WhyChooseUs from '@/components/HomePage/WhyChooseUs';
 import Cta2 from '@/components/cta/Cta2';
 import Usp from '@/components/Usp';
-import Testimonial from '@/components/testimonials/Testimonial1';
+// import Testimonial from '@/components/testimonials/Testimonial1';
 import Faq from '@/components/Faq';
 
 export default function Home() {
   return (
     <>
-      <HomeHero />
+      {/* <HomeHero /> */}
       <CoreServices/>
-      <CaseStudy/>
+      {/* <CaseStudy/> */}
       <Brands/>
       <Cta1/>
-      <Portfolio1/>
+      {/* <Portfolio1/> */}
       <AdvanceTech/>
       <Industries/>
       <QuickInsight/>
       <WhyChooseUs/>
       <Cta2/>
       <Usp/>
-      <Testimonial/>
+      {/* <Testimonial/> */}
       <Faq/>
     </>
   );
