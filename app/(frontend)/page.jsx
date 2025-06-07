73a25824a98f55d3f581b89fed18680cb44e8523
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2025-06-08 01:23:24 +0530

    Update page.jsx

--- app/(frontend)/page.jsx
+++ app/(frontend)/page.jsx
@@ -7,13 +7,13 @@ const HomeHero = dynamic(() => import('@/components/HomeHero'), {
 })
 import CoreServices from '@/components/HomePage/CoreServices';
 const CaseStudy = dynamic(() => import('@/components/HomePage/CaseStudy'), {
-  loading: () =>  <div className="flex justify-center items-center w-full h-[795px] bg-gray-300 animate-pulse"></div>
+  loading: () =>  <div className="flex justify-center items-center w-full min-h-[519.33px] bg-gray-300 animate-pulse"></div>
 })
 
 import Brands from '@/components/HomePage/Brands';
 import Cta1 from '@/components/cta/Cta1';
 const Portfolio1 = dynamic(() => import('@/components/portfolios/Portfolio1'), {
-  loading: () =>  <div className="flex justify-center items-center w-full h-[795px] bg-gray-300 animate-pulse"></div>
+  loading: () =>  <div className="flex justify-center items-center w-full min-h-[436px] bg-gray-300 animate-pulse"></div>
 })
 import AdvanceTech from '@/components/HomePage/AdvanceTech';
 import Industries from '@/components/HomePage/Industries';
@@ -22,7 +22,7 @@ import WhyChooseUs from '@/components/HomePage/WhyChooseUs';
 import Cta2 from '@/components/cta/Cta2';
 import Usp from '@/components/Usp';
 const Testimonial = dynamic(() => import('@/components/testimonials/Testimonial1'), {
-  loading: () =>  <div className="flex justify-center items-center w-full h-[795px] bg-gray-300 animate-pulse"></div>
+  loading: () =>  <div className="flex justify-center items-center w-full h-[565.39px] bg-gray-300 animate-pulse"></div>
 })
 import Faq from '@/components/Faq';
 import HomeHeroSkeleton from '@/components/skeleton/HomeHeroSkeleton';
