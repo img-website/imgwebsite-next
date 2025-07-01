6798593d413946b91342f90c940eb2f46911df76
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2025-07-01 12:13:32 +0530

    Allow phone lookup and handle Redis errors

--- middleware.js
+++ middleware.js
@@ -48,7 +48,7 @@ export async function middleware(request) {
   response.headers.set('X-Content-Type-Options', 'nosniff');
   response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
   response.headers.set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
-  response.headers.set('Content-Security-Policy', "default-src 'self' https://cdn.tiny.cloud; img-src 'self' https://cdn.tiny.cloud blob: data: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.tiny.cloud; style-src 'self' 'unsafe-inline' https://cdn.tiny.cloud; font-src 'self' 'unsafe-inline' data: https://cdn.tiny.cloud;");
+  response.headers.set('Content-Security-Policy', "default-src 'self' https://cdn.tiny.cloud; connect-src 'self' https://ipinfo.io; img-src 'self' https://cdn.tiny.cloud blob: data: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.tiny.cloud; style-src 'self' 'unsafe-inline' https://cdn.tiny.cloud; font-src 'self' 'unsafe-inline' data: https://cdn.tiny.cloud;");
   response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
 
   const { pathname, search, origin } = request.nextUrl;
