4da23a2d54cdf813fab4ee6cf80c06fcb59f090e
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2025-07-01 19:38:50 +0530

    jk

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,14 +1,9 @@
+import { SectionCards } from "@/components/section-cards";
 
 export default function Page() {
   return (
-    <>
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-2">
-        <div className="space-y-1">
-          <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-          <p className="text-muted-foreground">Welcome to your dashboard</p>
-        </div>
-        
-      </div>
-    </>
+    <div className="p-4">
+      <SectionCards/>
+    </div>
   );
 }
