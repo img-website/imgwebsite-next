7ea52ee47ee8c85ee23771284cf20175c12f4b8b
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2025-07-01 23:35:11 +0530

    jk

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,11 +1,13 @@
 import { SectionCards } from "@/components/section-cards";
 import { getDashboardStats } from "@/app/actions/dashboard";
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 
 export default async function Page() {
   const stats = await getDashboardStats();
   return (
-    <div className="p-4">
+    <div className="grid gap-4 p-4">
       <SectionCards stats={stats} />
+      <ChartAreaInteractive />
     </div>
   );
 }
