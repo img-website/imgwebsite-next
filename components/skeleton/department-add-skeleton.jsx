b4dd8a04a2f970fddbab260b63467e7b49139a92
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2025-07-04 11:04:40 +0530

    Improve department form skeletons

--- components/skeleton/department-add-skeleton.jsx
+++ components/skeleton/department-add-skeleton.jsx
@@ -1,6 +1,20 @@
 "use client";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function DepartmentAddSkeleton() {
   return (
@@ -21,8 +35,31 @@ export default function DepartmentAddSkeleton() {
               <Skeleton className="h-10 w-full" />
             </div>
             <div className="space-y-2">
-              <Skeleton className="h-4 w-32" />
-              <Skeleton className="h-10 w-full" />
+              <Skeleton className="h-4 w-40" />
+              <div className="rounded-md border overflow-x-auto">
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      {[...Array(6)].map((_, i) => (
+                        <TableHead key={i}>
+                          <Skeleton className="h-4 w-full" />
+                        </TableHead>
+                      ))}
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {[...Array(5)].map((_, row) => (
+                      <TableRow key={row}>
+                        {[...Array(6)].map((_, cell) => (
+                          <TableCell key={cell}>
+                            <Skeleton className="h-4 w-full" />
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </div>
             </div>
             <div className="flex justify-end gap-4">
               <Skeleton className="h-10 w-32" />
