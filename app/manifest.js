9b4273724e134eb2c91af7ba7d7266ca63b6903c
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2025-07-07 17:14:55 +0530

    Update manifest.js

--- app/manifest.js
+++ app/manifest.js
@@ -11,6 +11,49 @@ export default function manifest() {
     lang: 'en-US',
     orientation: 'portrait',
     prefer_related_applications: false,
+    screenshots: [
+      {
+        src: "https://d1y41eupgbwbb2.cloudfront.net/images/heroBot-768.webp",
+        sizes: "1080x1920",
+        type: "image/webp"
+      },
+      {
+        src: "https://d1y41eupgbwbb2.cloudfront.net/images/FuturisticAIPoweredMobileApps.webp",
+        sizes: "1080x1920",
+        type: "image/webp"
+      },
+      {
+        src: "https://d1y41eupgbwbb2.cloudfront.net/images/cta1Image.webp",
+        sizes: "1080x1920",
+        type: "image/webp"
+      }
+    ],
+    shortcuts: [
+      {
+        name: 'Home',
+        url: '/',
+        icons: [
+          {
+            src: '/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
+      },
+      {
+        name: 'Contact Us',
+        url: '/contact-us',
+        icons: [
+          {
+            src: '/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
+      },
+    ],
     icons: [
       {
         src: '/android-chrome-192x192.png',
