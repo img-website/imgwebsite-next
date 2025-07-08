1753128d2fe058f6e3eb337666f9d5cd41083a96
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2025-07-08 10:57:44 +0530

    Ensure status and background color fields load

--- app/admin/blogs/[id]/edit/page.jsx
+++ app/admin/blogs/[id]/edit/page.jsx
@@ -299,6 +299,8 @@ export default function Page({ params }) {
       bgColorStatus: blog.bg_color_status ?? false,
       bgColor: blog.bg_color || "",
     });
+    form.setValue("status", String(blog.status ?? 1));
+    form.setValue("bgColorStatus", blog.bg_color_status ?? false);
     setStatus(String(blog.status ?? 1));
     setBgColorStatusValue(blog.bg_color_status ?? false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
