07b8d4e08dad8806970d9050e8802f7af4892120
Jitendra Kumar Bijarnia <67179546+img-website@users.noreply.github.com>
2025-07-23 13:32:09 +0530

    fix build warnings

--- helpers/getStaticMeta.js
+++ helpers/getStaticMeta.js
@@ -2,7 +2,7 @@ export default async function getStaticMeta() {
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/admin/meta/static`,
-      { cache: 'no-store' }
+      { next: { revalidate: 3600 } }
     );
     const json = await res.json();
     if (json.success && json.data) {
